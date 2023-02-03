b5ce052a76a3a32bbc8f3f77e7163e24bdd995a2
vivimila <violamilashevskaya@yandex.by>
2023-02-03 13:13:07 +0300

    Create a HashSet

--- Program.java
+++ Program.java
@@ -1,3 +1,22 @@
 // Разработать программу, имитирующую поведение коллекции HashSet. 
 // В программе содать метод add добавляющий элемент, и метод позволяющий выводить эллементы коллекции в консоль. 
 // Формат данных Integer.
+
+import java.util.HashSet;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        HashSet<Integer> set = new HashSet<>();
+        set.add(11);
+        set.add(22);
+        set.add(33);
+        set.add(44);
+        set.add(55);
+        //set.add(22);
+ 
+        System.out.println("HashSet : " + set);
+              
+    }  
+}
+        
