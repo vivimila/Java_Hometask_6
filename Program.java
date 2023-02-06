6d3dd2043a1c5a45f62985efb8a48d2311ca6e3b
vivimila <violamilashevskaya@yandex.by>
2023-02-06 16:45:38 +0300

    Work with HashMap

--- Program.java
+++ Program.java
@@ -2,21 +2,35 @@
 // В программе содать метод add добавляющий элемент, и метод позволяющий выводить эллементы коллекции в консоль. 
 // Формат данных Integer.
 
-import java.util.HashSet;
+// (Нужно написать пару методов. Один будет добавлять элементы по правилам хэшсета,
+// а второй работать как итератор или просто возвращать по индексу. 
+// Но всё это средствами HashMap!)
+
+import java.util.HashMap;
+//import java.util.HashSet;
+import java.util.Set;
 
 public class Program {
-    public static void main(String[] args) {
-        
-        HashSet<Integer> set = new HashSet<>();
-        set.add(11);
-        set.add(22);
-        set.add(33);
-        set.add(44);
-        set.add(55);
-        //set.add(22);
- 
-        System.out.println("HashSet : " + set);
-              
-    }  
-}
+    private static HashMap <Integer, Object> hashMap = new HashMap<>();
+    private static final Object PRESENT  = new Object();
+    public static void main(String[] args) throws Exception {
+        add(11);
+        add(22);
+        add(33);
+        add(44);
+        add(55);
+        // System.out.println(hashMap.keySet());
+    }
+
+    private static Set<Integer> getString(){
+        return (hashMap.keySet()) ;
+        System.out.println(hashMap.keySet());
+    }
+
+    public static void add(Integer numbers) {
+        hashMap.put(numbers, PRESENT);
+    } 
+               
+}  
+
         
